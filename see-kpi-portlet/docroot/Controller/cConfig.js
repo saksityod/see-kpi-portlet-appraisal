f171167661eb69292203ad77183b7e4dd14645f5
SRIKOT <srikott@gmail.com>
2018-01-29 15:43:25 +0700

    project see-kpi-portlet-appraisal

---
+++ see-kpi-portlet/docroot/Controller/cConfig.js
@@ -0,0 +1,18 @@
+var restfulURL="";
+var serviceName="see_api";
+/*#######Office#######*/
+
+//restfulURL="http://192.168.1.254";
+
+/*#######Localhost#######*/
+//restfulURL="http://192.168.1.59";
+restfulURL="http://localhost";
+
+/*#######Poroduction#######*/
+//restfulURL="https://ghbank-smart-radar.ghb.co.th";
+
+/*#######Cloud#######*/
+//restfulURL="http://210.4.153.18";
+
+/*#######PJoe######*/
+//restfulURL="http://192.168.43.118";
